f31a03d2e96994a7aa06ca9c74152c4131a4c314
Vuko Vukasinovic <vukasinovicvuko5@gmail.com>
2024-05-22 14:59:49 +0200

    skeleton of product info

---
+++ assets/product-slider.js
@@ -0,0 +1,19 @@
+var swiperThumbs = new Swiper('.product__swiper--thumbs', {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var swiperMain = new Swiper('.product__swiper-main', {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.product__swiper-button-next',
+    prevEl: '.product__swiper-button-prev',
+  },
+  thumbs: {
+    swiper: swiperThumbs,
+  },
+});
